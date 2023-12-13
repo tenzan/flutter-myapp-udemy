0da00d04c25dcb1c1a91c94bb08591aaf1d16feb
Askar <askar.karasaev@gmail.com>
2023-12-13 14:47:29 +0900

    102.HTTP Request via User Form

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,18 @@ class Album {
   final int id;
   final String title;
 
-  const Album(
-      {required this.userId, required this.id, required this.title});
+  const Album({required this.userId, required this.id, required this.title});
 
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
-        userId: json['userId'],id: json['id'], title: json['title']);
+        userId: json['userId'], id: json['id'], title: json['title']);
   }
 }
 
-Future<Album> fetchAlbum() async {
+Future<Album> fetchAlbum(int albumId) async {
   final response =
-      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/2'));
+  await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'));
 
   if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
@@ -39,31 +39,57 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late Future<Album> futureAlbum;
+  final TextEditingController _controller = TextEditingController();
+
+  void _fetchAlbum() {
+    int albumId = int.tryParse(_controller.text) ?? 1;
+    setState(() {
+      futureAlbum = fetchAlbum(albumId);
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureAlbum = fetchAlbum(1);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'demo',
-        theme: ThemeData(primarySwatch: Colors.green),
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Demo')),
-            body: Center(
-              child: FutureBuilder<Album>(
-                  future: futureAlbum,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Text(snapshot.data!.title);
-                    } else if (snapshot.hasError) {
-                      return Text('${snapshot.error}');
+      title: 'demo',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Demo')),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      labelText: 'Album ID Number'),
+                ),
+                ElevatedButton(
+                    onPressed: _fetchAlbum,
+                    child: const Text('Submit')
+                ),
+                FutureBuilder(
+                    future: futureAlbum,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Text(snapshot.data!.title);
+                      } else if (snapshot.hasError) {
+                        return Text('{snapshot.erro}');
+                      }
+                      return const CircularProgressIndicator();
                     }
-                    return const CircularProgressIndicator();
-                  }),
-            )));
+                ),
+              ],
+            )
+        ),
+      ),
+    );
   }
 }
