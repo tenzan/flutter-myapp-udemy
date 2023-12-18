41a721e749446eb23b169e74af0fa26eadf9c2b9
Askar <askar.karasaev@gmail.com>
2023-12-18 21:57:38 +0900

    adding assets

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,7 +5,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('HOME SCREEN')),
-      body: Center(child: Text('Home screen, inside home_screent.dart')),
+      body: Center(
+        child: Image.asset(
+            'assets/flowers.jpg'),
+      ),
     );
   }
 }
