aa4895826fe36a6a67c92a88b04a5c9fa6de9e3f
Askar <askar.karasaev@gmail.com>
2023-12-18 19:47:44 +0900

    separating files

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,16 @@
-import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:my_app/home_screen.dart';
 
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  const Album({required this.userId, required this.id, required this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-        userId: json['userId'], id: json['id'], title: json['title']);
-  }
+void main() {
+  runApp(MyApp());
 }
 
-Future<Album> fetchAlbum(int albumId) async {
-  final response =
-  await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'));
-
-  if (response.statusCode == 200) {
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failure to fetch data!');
-  }
-}
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
-  final TextEditingController _controller = TextEditingController();
-
-  void _fetchAlbum() {
-    int albumId = int.tryParse(_controller.text) ?? 1;
-    setState(() {
-      futureAlbum = fetchAlbum(albumId);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum(1);
-  }
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'demo',
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Demo')),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: _controller,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                      labelText: 'Album ID Number'),
-                ),
-                ElevatedButton(
-                    onPressed: _fetchAlbum,
-                    child: const Text('Submit')
-                ),
-                FutureBuilder(
-                    future: futureAlbum,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Text(snapshot.data!.title);
-                      } else if (snapshot.hasError) {
-                        return Text('{snapshot.erro}');
-                      }
-                      return const CircularProgressIndicator();
-                    }
-                ),
-              ],
-            )
-        ),
-      ),
+      title: 'my app',
+      home: HomeScreen(),
     );
   }
 }
